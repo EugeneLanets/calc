b1f23aee0176935ec7c407174e82bfe57c304f3c
Eugene Lanets <swarrt@yandex.ru>
2024-08-18 22:40:12 +0300

    feat: add basic keyboard grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Container, TextField } from "@mui/material";
+import { Button, Container, TextField } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2";
 
 function App() {
   return (
-    <Container sx={{ backgroundColor: "blue", p: 10, textAlign: "right" }}>
+    <Container
+      sx={{ p: 10, border: "1px solid black", borderRadius: 5 }}
+      maxWidth={"sm"}
+    >
       <TextField
         variant="outlined"
-        label="0"
         fullWidth
         InputProps={{
           sx: {
@@ -15,6 +18,29 @@ function App() {
           },
         }}
       />
+      <Grid2 container spacing={3} columns={4}>
+        <Grid2
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          md={1}
+        >
+          <Button variant={"contained"} fullWidth>
+            7
+          </Button>
+        </Grid2>
+        <Grid2 md={1}>8</Grid2>
+        <Grid2 md={1}>9</Grid2>
+        <Grid2 md={1}>+</Grid2>
+        <Grid2 md={1}>4</Grid2>
+        <Grid2 md={1}>5</Grid2>
+        <Grid2 md={1}>6</Grid2>
+        <Grid2 md={1}>-</Grid2>
+        <Grid2 md={1}>1</Grid2>
+        <Grid2 md={1}>2</Grid2>
+        <Grid2 md={1}>3</Grid2>
+        <Grid2 md={1}>=</Grid2>
+      </Grid2>
     </Container>
   );
 }
