0dceb2400b518c87ed5c1465f2c9413e90d5c2f7
Eugene Lanets <mrshaftoe@gmail.com>
2024-08-19 14:57:10 +0300

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -18,24 +18,25 @@ function App() {
             },
           },
         }}
+        sx={{ marginBottom: 5 }}
       />
       <Grid2 container spacing={3} columns={4}>
         <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
-        <Button value={7} />
+        <Button value={8} />
+        <Button value={9} />
+        <Button value={0} />
+        <Button value={4} />
+        <Button value={5} />
+        <Button value={6} />
+        <Button value={0} />
+        <Button value={1} />
+        <Button value={2} />
+        <Button value={3} />
+        <Button value={0} />
+        <Button value={0} />
+        <Button value={0} />
+        <Button value={0} />
+        <Button value={0} />
       </Grid2>
     </Container>
   );
